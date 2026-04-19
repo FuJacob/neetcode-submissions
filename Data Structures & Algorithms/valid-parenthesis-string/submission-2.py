d9fbc3f5159fc63cf01a360e00e8efe9710fa732
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2026-04-19 14:10:12 -0400

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        ## low and high reprensts range of unmatched
+        low = high = 0
+        for char in s:
+            if char == "(":
+                low +=1
+                high +=1
+            elif char == ")":
+                low -=1
+                high -=1
+                # could close one, coudl add another unmatched
+            else:
+                low -=1
+                high +=1
+            if high < 0:
+                return False
+            low = max(0, low)
+        return low == 0
+            
+
+
+
+
+
+
