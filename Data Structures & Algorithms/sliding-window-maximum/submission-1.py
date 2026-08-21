326810ea4a856c04f3f04306c87198877af0d204
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2026-08-21 13:17:47 -0400

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+         maximum element in the window
+         lazy deetion
+         1 heaps
+         heap  -> biggest elem -> + idx
+         (1,0)
+         (2,1)
+        """
+        delete = set()
+        n = len(nums)
+        heap = []
+        left = 0
+        ans = []
+        for right, num in enumerate(nums):
+            ## push crr
+            heapq.heappush(heap, (-num, right))
+
+            window_length = right - left + 1
+            if window_length < k:
+                continue
+            if window_length > k:
+                delete.add((-nums[left], left))
+                left+=1
+            ## lazy edleetions
+            while heap and heap[0] in delete:
+                heapq.heappop(heap)
+            ans.append(-heap[0][0])
+        return ans
+
+
