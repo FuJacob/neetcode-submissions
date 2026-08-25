e4730d158b296d799933e785876adbff84160c08
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2026-08-25 13:46:24 -0400

    Add: roman-to-integer - submission-2

---
+++ Data Structures & Algorithms/roman-to-integer/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        sym_to_val = {
+        'I': 1,
+        'V': 5, 
+        'X': 10, 
+        'L': 50, 
+        'C': 100, 
+        'D': 500, 
+        'M': 1000, 
+        } 
+        n = len(s)
+        for i in range(n-1,-1,-1):
+            ## resvresd
+            curr = sym_to_val[s[i]]
+            if i < n - 1 and sym_to_val[s[i+1]] > curr:
+                ans -= curr
+            else:
+                ans += curr
+        return ans
+
+            
+
