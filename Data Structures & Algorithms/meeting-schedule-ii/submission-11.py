94201af37284e89981ed6e7b19ec794b2ca648db
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2026-08-27 14:37:31 -0400

    Add: meeting-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-11.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        ans = 0
+        time_to_delta = defaultdict(int)
+        for interval in intervals:
+            time_to_delta[interval.start] += 1
+            time_to_delta[interval.end] -= 1
+        curr = 0
+        events = sorted(time_to_delta.items())
+        for _, change in events:
+            curr += change
+            ans = max(curr, ans)
+        return ans
+
+
+        
+
